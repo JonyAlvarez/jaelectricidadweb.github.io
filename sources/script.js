f4c9ec4f5a704c7d2263ef8b1cb48b510b6027f1
Jonathan ezequiel alvarez <jonathandevelop3r@gmail.com>
2025-10-26 20:27:43 -0300

    segunda subida

--- sources/script.js
+++ sources/script.js
@@ -51,3 +51,69 @@ function Wbutton(){
 });
 
 }
+
+
+
+function closeCarousel(){
+
+
+document.addEventListener('DOMContentLoaded', () => {
+  const modalEl = document.getElementById('imageModal');
+  const modalImage = document.getElementById('modalImage');
+  const bsModal = new bootstrap.Modal(modalEl);
+
+  document.querySelectorAll('.brand-carousel-track img').forEach(img => {
+    img.addEventListener('click', () => {
+      modalImage.src = img.dataset.full || img.src;
+      bsModal.show();
+    });
+  });
+
+  modalEl.addEventListener('hidden.bs.modal', () => {
+    modalImage.src = '';
+  });
+});
+
+
+document.addEventListener('DOMContentLoaded', () => {
+  const modalEl = document.getElementById('imageModal');
+  const modalImage = document.getElementById('modalImage');
+  const closeBtn = modalEl.querySelector('.btn-close-custom');
+  const bsModal = new bootstrap.Modal(modalEl);
+
+  document.querySelectorAll('.brand-carousel-track img').forEach(img => {
+    img.addEventListener('click', () => {
+      modalImage.src = img.src;
+      bsModal.show();
+
+      // Cargar la imagen para detectar brillo promedio
+      const tempImg = new Image();
+      tempImg.crossOrigin = 'anonymous';
+      tempImg.src = img.src;
+      tempImg.onload = function () {
+        const canvas = document.createElement('canvas');
+        const ctx = canvas.getContext('2d');
+        canvas.width = this.width;
+        canvas.height = this.height;
+        ctx.drawImage(this, 0, 0);
+        const data = ctx.getImageData(0, 0, canvas.width, canvas.height).data;
+        let sum = 0;
+        for (let i = 0; i < data.length; i += 4) {
+          sum += (data[i] + data[i + 1] + data[i + 2]) / 3;
+        }
+        const brightness = sum / (data.length / 4);
+        if (brightness > 128) {
+          closeBtn.classList.add('light-bg');
+        } else {
+          closeBtn.classList.remove('light-bg');
+        }
+      };
+    });
+  });
+
+  modalEl.addEventListener('hidden.bs.modal', () => {
+    modalImage.src = '';
+  });
+});
+
+}
